65a9c9e35e7d0425561496ec3e7874a0648781c7
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-01-31 17:37:31 +0100

    1.04

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,18 +13,18 @@
             image: 'src/scss/images/slot-machine.png'
         },
         {
-            name: 'Diamond',
-            density: 3.5,
+            name: 'Ruby',
+            density: 4.1,
             image: 'src/scss/images/precious-stone.png'
         },
         {
-            name: 'Diamond',
-            density: 3.5,
+            name: 'Emerald',
+            density: 2.7,
             image: 'src/scss/images/gemstone.png'
         },
         {
-            name: 'Diamond',
-            density: 3.5,
+            name: 'Orange Sapphire',
+            density: 3.9,
             image: 'src/scss/images/gem.png'
         }
 ];
